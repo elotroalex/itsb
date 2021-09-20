b676e5afda2848e462187f6f2d3a4fefd9e5eb72
Alex Gil <colibri.alex@gmail.com>
2021-09-20 16:28:39 -0400

    installed new eslint

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,9 +1,10 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import { StaticQuery, graphql } from 'gatsby'
+import React from "react";
+import PropTypes from "prop-types";
+import { StaticQuery, graphql } from "gatsby";
 
-import Header from './header'
-import './layout.css'
+import Header from "./header";
+import "./layout.css";
+import Navbar from "./navbar";
 
 const Layout = ({ children }) => (
   <StaticQuery
@@ -16,14 +17,15 @@ const Layout = ({ children }) => (
         }
       }
     `}
-    render={data => (
+    render={(data) => (
       <>
         <Header siteTitle={data.site.siteMetadata.title} />
+        <Navbar />
         <div
           style={{
-            margin: '0 auto',
+            margin: "0 auto",
             maxWidth: 960,
-            padding: '0px 1.0875rem 1.45rem',
+            padding: "0px 1.0875rem 1.45rem",
             paddingTop: 0,
           }}
         >
@@ -32,10 +34,10 @@ const Layout = ({ children }) => (
       </>
     )}
   />
-)
+);
 
 Layout.propTypes = {
   children: PropTypes.node.isRequired,
-}
+};
 
-export default Layout
+export default Layout;
