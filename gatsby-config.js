a512efb47e011c7ddd9a2a23bbe33bf865ef701c
Alex Gil <colibri.alex@gmail.com>
2021-09-20 17:20:36 -0400

    loaded simple gif into instruction

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,20 +1,19 @@
-const path = require('path')
+const path = require("path");
 
 module.exports = {
   siteMetadata: {
-    title: 'In the Same Boats',
-    description:
-      'The best data project evah.',
-    author: '@elotroalex & @muziejus',
+    title: "In the Same Boats",
+    description: "The best data project evah.",
+    author: "@elotroalex & @muziejus",
   },
   plugins: [
     {
       resolve: `gatsby-plugin-mdx`,
       options: {
-        defaultLayouts: { default: path.resolve('./src/components/layout.js') },
+        defaultLayouts: { default: path.resolve("./src/components/layout.js") },
       },
     },
-    'gatsby-plugin-react-helmet',
+    "gatsby-plugin-react-helmet",
     {
       resolve: `gatsby-source-filesystem`,
       options: {
@@ -22,22 +21,24 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
-    'gatsby-transformer-sharp',
-    'gatsby-plugin-sharp',
+    "gatsby-plugin-image",
+    "gatsby-transformer-sharp",
+    "gatsby-plugin-sharp",
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'gatsby-default-mdx-basic',
-        short_name: 'starter',
-        start_url: '/',
-        background_color: '#663399',
-        theme_color: '#663399',
-        display: 'minimal-ui',
-        icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
+        name: "gatsby-default-mdx-basic",
+        short_name: "starter",
+        start_url: "/",
+        background_color: "#663399",
+        theme_color: "#663399",
+        display: "minimal-ui",
+        icon: "src/images/gatsby-icon.png", // This path is relative to the root of the site.
       },
     },
+
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.app/offline
     // 'gatsby-plugin-offline',
   ],
-}
+};
---
+++ src/components/images/sample.js
@@ -0,0 +1,11 @@
+import { StaticImage } from "gatsby-plugin-image";
+import React from "react";
+
+export default function Sample() {
+  return (
+    <StaticImage
+      src="../../images/sample.png"
+      alt="Demonstration of trajectories"
+    />
+  );
+}
