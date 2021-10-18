c47784b3c768ca45ff4e810aeb62ab421ca13038
Alex Gil <colibri.alex@gmail.com>
2021-10-18 16:16:11 -0400

    modularized some and flex

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,18 +1,36 @@
 import React from "react";
 import { Link } from "gatsby";
 import "./navbar.css";
+import Header from "./header";
+import { StaticQuery, graphql } from "gatsby";
 
 export default function Navbar() {
   return (
-    <nav style={{ paddingBottom: "2rem" }}>
-      <div className="links">
-        <Link to="/">Home</Link>
-        <Link to="/instructions">Instructions</Link>
-        <Link to="/trajectories">Trajectories</Link>
-        <Link to="/intersections">Intersections</Link>
-        <Link to="/search">Search</Link>
-        <Link to="/credits">Credits</Link>
-      </div>
-    </nav>
+    <StaticQuery
+      query={graphql`
+        query SiteTitleQuery {
+          site {
+            siteMetadata {
+              title
+            }
+          }
+        }
+      `}
+      render={(data) => (
+        <div className="navbar">
+          <Header siteTitle={data.site.siteMetadata.title} />
+          <nav className="navigation" style={{ paddingBottom: "2rem" }}>
+            <div className="links">
+              <Link to="/">Home</Link>
+              <Link to="/instructions">Instructions</Link>
+              <Link to="/trajectories">Trajectories</Link>
+              <Link to="/intersections">Intersections</Link>
+              <Link to="/search">Search</Link>
+              <Link to="/credits">Credits</Link>
+            </div>
+          </nav>
+        </div>
+      )}
+    />
   );
 }
--- src/components/header.js
+++ src/components/header.js
@@ -2,57 +2,20 @@ import { Link } from "gatsby";
 import PropTypes from "prop-types";
 import React from "react";
 import itsbIcon from "../images/itsb_icon.svg";
+import "./header.css";
 
 const Header = ({ siteTitle }) => (
-  <div
-    style={{
-      background: "#fcb040",
-      marginBottom: "1.45rem",
-      display: "flex",
-      width: "300px",
-      height: "70px",
-    }}
-  >
-    <div
-      style={{
-        margin: "0 auto",
-        maxWidth: 300,
-        padding: "1rem",
-      }}
-    >
-      <Link
-        to="/"
-        style={{
-          color: "black",
-          textDecoration: "none",
-          fontSize: "1rem",
-          display: "flex",
-        }}
-      >
-        <figure
-          style={{
-            display: "flex",
-            marginBottom: 0,
-          }}
-        >
-          <img
-            src={itsbIcon}
-            alt="ITSB Icon: A concentric circle."
-            style={{
-              width: "1rem",
-            }}
-          />
-        </figure>
-        <span
-          style={{
-            margin: 0,
-            textTransform: "uppercase",
-          }}
-        >
-          {siteTitle}
-        </span>
-      </Link>
-    </div>
+  <div className="title">
+    <Link to="/">
+      <figure>
+        <img
+          className="logo"
+          src={itsbIcon}
+          alt="ITSB Icon: A concentric circle."
+        />
+      </figure>
+      <span>{siteTitle}</span>
+    </Link>
   </div>
 );
 
